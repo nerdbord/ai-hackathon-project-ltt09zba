c4b881924a0b0120ee948656a1b8a06e401237b1
Szymon Kedra <s5zee@wp.pl>
2024-03-17 12:51:22 +0100

    wrapping checkbox to next line when no space is available

--- agile-guardians/client/app/components/SearchForm/CheckboxContainer/index.tsx
+++ agile-guardians/client/app/components/SearchForm/CheckboxContainer/index.tsx
@@ -10,7 +10,7 @@ const CheckboxContainer = ({ children, error }: CheckboxContainerProps) => {
   return (
     <div className="mb-[52px] flex flex-col gap-2">
       <p className="text-2xl font-normal ">Sprawdź produkt pod kątem:</p>
-      <div className="flex gap-2">{children}</div>
+      <div className="flex flex-wrap gap-2">{children}</div>
       {error && (
         <div className="grid h-[32px] items-center rounded-lg bg-errorLight pl-3 sm:w-[320px]">
           <p role="alert" className="flex gap-1 text-sm font-normal text-error">
